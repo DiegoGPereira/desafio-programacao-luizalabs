c7f54c179088798a8481b4aa9d93de8aadc161c8
Diego Guedes Pereira <diegoguedespereira@gmail.com>
2024-09-05 12:18:16 -0300

    feat: subindo arquivos

---
+++ src/main/java/br/com/magazineluiza/desafio_programacao_luizalabs/DesafioProgramacaoLuizalabsApplication.java
@@ -0,0 +1,13 @@
+package br.com.magazineluiza.desafio_programacao_luizalabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProgramacaoLuizalabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProgramacaoLuizalabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/magazineluiza/desafio_programacao_luizalabs/DesafioProgramacaoLuizalabsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.magazineluiza.desafio_programacao_luizalabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProgramacaoLuizalabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
